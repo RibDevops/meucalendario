fdd9d3d379c31bc5420feb8d87ae36991901c12d
Ribeiro <ribeiro.ajr@icloud.com>
2026-07-16 16:31:47 -0300

    v123

--- core/settings.py
+++ core/settings.py
@@ -23,7 +23,7 @@ DEBUG = env_bool('DJANGO_DEBUG', True)
 
 ALLOWED_HOSTS = env_list(
     'DJANGO_ALLOWED_HOSTS',
-    '127.0.0.1,localhost' if DEBUG else 'agenda.pythonanywhere.com',
+    '127.0.0.1,localhost,agenda.pythonanywhere.com',
 )
 
 CSRF_TRUSTED_ORIGINS = env_list(
